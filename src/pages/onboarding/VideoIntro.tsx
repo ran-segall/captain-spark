41a509d44d252f576d62605b5468ddec33621b26
Ran <ransegall@gmail.com>
2025-07-06 15:10:25 +0700

    Onboarding Video Plays (no react bug)

--- src/pages/onboarding/VideoIntro.tsx
+++ src/pages/onboarding/VideoIntro.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import AppLayout from '../../components/ScreenLayout';
 import VideoPlayer from '../../components/VideoPlayer';
