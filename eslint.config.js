edfa9de22b8b6a78fa80081bcaa80e714f6f351b
Ran <ransegall@gmail.com>
2025-07-07 16:13:20 +0700

    add pre-commit lint error catching

--- eslint.config.js
+++ eslint.config.js
@@ -19,5 +19,9 @@ export default tseslint.config([
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    rules: {
+      'no-unused-vars': ['error', { vars: 'all', args: 'after-used', ignoreRestSiblings: false }],
+      '@typescript-eslint/no-unused-vars': ['error'],
+    },
   },
 ])
